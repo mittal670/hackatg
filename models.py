5e89017bb70104a0118b85ac538e55ed5ed6d2e1
mittal670 <250160450030.gvp@gujaratvidyapith.org>
2026-02-21 17:37:44 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,11 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    ROLE_CHOICES = (
-        ('manager', 'Fleet Manager'),
-        ('dispatcher', 'Dispatcher'),
-        ('safety', 'Safety Officer'),
-        ('finance', 'Financial Analyst'),
-    )
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+# Create your models here.
--- views.py
+++ views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+def vehicle_roi(vehicle):
+    revenue = 100000
+    maintenance_cost = vehicle.servicelog_set.aggregate(models.Sum('cost'))['cost__sum'] or 0
+    fuel_cost = vehicle.fuellog_set.aggregate(models.Sum('cost'))['cost__sum'] or 0
 
-# Create your views here.
+    acquisition_cost = 500000
+
+    roi = (revenue - (maintenance_cost + fuel_cost)) / acquisition_cost
+    return roi
